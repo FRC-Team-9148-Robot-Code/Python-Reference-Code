933e2947df6c791bbd4d713658ea5f445cc544b8
IanSpurlock <ianspurlock1@gmail.com>
2026-06-18 17:51:26 -0400

    First example

---
+++ 2027Example9148/robot.py
@@ -0,0 +1,18 @@
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+
+import wpilib
+import rev
+# todo: install
+class Robot(wpilib.TimedRobot):
+    def __init__(self):
+        super().__init__()
+
+        leftMotor = rev.SparkMax(1, rev.SparkLowLevel.MotorType.kBrushed)
+        rightMotor = rev.SparkMax(2, rev.SparkLowLevel.MotorType.kBrushed)
+        self.robotDrive = wpilib.DifferentialDrive(leftMotor, rightMotor)
+        self.driverController = wpilib.XboxController(0)
+
+    def teleopPeriodic(self):
+        self.robotDrive.tankDrive(-self.driverController.getLeftY(), -self.driverController.getRightY())
